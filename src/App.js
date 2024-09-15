236a840bd4b64b46454ce435ac5b31726f8d51b6
Nelvysz <new.nitipoomsomsakit@gmail.com>
2024-09-15 20:50:23 +0700

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,35 @@
 import logo from './logo.svg';
 import './App.css';
+import { GoogleLogin } from '@react-oauth/google';
 
 function App() {
+  const handleSuccess = (response) => {
+    console.log('Login Success:', response);
+    // Handle successful login here
+    // e.g., send response to your backend, store token, etc.
+  };
+
+  const handleError = (error) => {
+    console.error('Login Error:', error);
+    // Handle login error here
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="center-container">
+      <div className='centered-content'>
+        <img src="heart.png" width="100" alt="Heart" />
+        <h1 className='login'>Login</h1>
+        {/* <button className="button-google" id="googleLogin">Login using Google</button> */}
+        <GoogleLogin
+          onSuccess={handleSuccess}
+          onError={handleError}
+        />
+        <div className="separator"><span>or log in with your email</span></div>
+
+        <input type="email" id="email" className="input-field" placeholder="Email Address" />
+        <input type="password" id="password" className="input-field" placeholder="Password" />
+        <button id="loginButton" className="signin_button">Login</button>
+        <div className="signup-link">Don't have an account? <a href="signup.html">Sign Up</a></div>
+      </div>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -3,11 +3,15 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { GoogleOAuthProvider } from '@react-oauth/google';
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <GoogleOAuthProvider clientId={'21077986020-2gnfcprgb71q8n17t9kcnb6crt0dfcsg.apps.googleusercontent.com'}>
+      <App />
+    </GoogleOAuthProvider>
   </React.StrictMode>
 );
 
